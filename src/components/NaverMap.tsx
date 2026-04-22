9c5ce1552e404e9ae15fd54eeaebac218205020c
안지용 <agy612@gmail.com>
2026-04-22 20:35:17 +0900

    Update Naver Maps client ID

--- src/components/NaverMap.tsx
+++ src/components/NaverMap.tsx
@@ -33,7 +33,7 @@ export default function NaverMap() {
 
   useEffect(() => {
     let cancelled = false;
-    const clientId = '2dxl2whqyb';
+    const clientId = 'lsh4eg05nz';
 
     window.navermap_authFailure = () => {
       console.error('Naver Maps 인증 실패: Client ID를 확인해주세요.');
